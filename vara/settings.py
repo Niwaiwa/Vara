2cfa67bc68ccc5f2921f48225f390ef166eb5f31
Niwaiwa <po2846@gmail.com>
2023-03-21 01:20:49 +0800

    add database connection

--- vara/settings.py
+++ vara/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp.apps.MyappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'vara.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vara',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PORT': '3307',
+        'PASSWORD': 'password',
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        },
     }
 }
 
