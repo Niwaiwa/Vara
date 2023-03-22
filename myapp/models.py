8a648d91789edd1f374d66e395de2c7be5edf75b
Niwaiwa <po2846@gmail.com>
2023-03-22 23:30:51 +0800

    add video and tag table migration

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+class TagVideo(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
