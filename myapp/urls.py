e9d688cb706f0df92fbe235ca55e8e1dd937cb47
Niwaiwa <po2846@gmail.com>
2023-03-24 01:23:52 +0800

    add video detail api and tag detail api

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
-from myapp.views import video_list
+from myapp.views import video_list, video_detail, tag_detail
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('videos/', video_list, name='video_list'),
+    path('videos/<int:id>/', video_detail, name='video_detail'),
+    path('tags/<int:id>/', tag_detail, name='tag_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from django.http import HttpResponse
-from .models import Video
+from .models import Video, Tag, TagVideo
 
 
 def index(request):
@@ -10,3 +10,14 @@ def index(request):
 def video_list(request):
     videos = Video.objects.all()
     return render(request, 'myapp/video_list.html', {'videos': videos})
+
+def video_detail(request, id):
+    video = get_object_or_404(Video, id=id)
+    tags = Tag.objects.filter(tagvideo__video_id=id)
+    print(tags)
+    return render(request, 'myapp/video_detail.html', {'video': video, 'tags': tags})
+
+def tag_detail(request, id):
+    tag = get_object_or_404(Tag, id=id)
+    videos = Video.objects.filter(tagvideo__tag_id=id)
+    return render(request, 'myapp/tag_detail.html', {'videos': videos, 'tag': tag})
