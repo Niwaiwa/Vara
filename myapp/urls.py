7be3f100507053aeee13965233772f346a084ea1
Niwaiwa <po2846@gmail.com>
2023-03-25 03:45:43 +0800

    add login and user mypage
    add login, logout, register, mypage api and html and css

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from myapp.views import video_list, video_detail, tag_detail
+from myapp.views import video_list, video_detail, tag_detail, user_register, user_login, user_logout, mypage
 
 
 urlpatterns = [
@@ -9,4 +9,8 @@ urlpatterns = [
     path('videos/', video_list, name='video_list'),
     path('videos/<int:id>/', video_detail, name='video_detail'),
     path('tags/<int:id>/', tag_detail, name='tag_detail'),
+    path('register/', user_register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('mypage/', mypage, name='mypage'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from django.http import HttpResponse
-from .models import Video, Tag, TagVideo
+from .models import Video, Tag
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    if 'user' in request.session:
+        current_user = request.session['user']
+        param = {'current_user': current_user}
+    else:
+        param = {}
+    videos = Video.objects.all()
+    return render(request, 'base.html', param.update({'videos': videos}))
+    
 def video_list(request):
     videos = Video.objects.all()
     return render(request, 'myapp/video_list.html', {'videos': videos})
@@ -21,3 +30,39 @@ def tag_detail(request, id):
     tag = get_object_or_404(Tag, id=id)
     videos = Video.objects.filter(tagvideo__tag_id=id)
     return render(request, 'myapp/tag_detail.html', {'videos': videos, 'tag': tag})
+
+def user_register(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        pwd = request.POST.get('password')
+        email = request.POST.get('email')
+        if User.objects.filter(username=uname).count() > 0:
+            return HttpResponse('Username already exists.')
+        else:
+            _ = User.objects.create_user(uname, email, pwd)
+            return redirect('login')
+    else:
+        return render(request, 'myapp/register.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        pwd = request.POST.get('password')
+
+        check_user = authenticate(request, username=uname, password=pwd)
+        if check_user:
+            login(request, check_user)
+            return redirect('/myapp/videos/')
+        else:
+            return HttpResponse('Please enter valid Username or Password.')
+
+    return render(request, 'myapp/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='/myapp/login/')
+def mypage(request):
+    user = request.user
+    return render(request, 'myapp/mypage.html', {'user': user})
