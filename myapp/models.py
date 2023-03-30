bbae72175c6f7340a5044116d7c92d8cd230a8e9
Niwaiwa <po2846@gmail.com>
2023-03-31 01:56:19 +0800

    change image file to use unique filename

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+def unique_filename(path):
+    """
+    Enforce unique upload file names.
+    Usage:
+    class MyModel(models.Model):
+        file = ImageField(upload_to=unique_filename("path/to/upload/dir"))
+    """
+    import os, base64, uuid
+    def _func(instance, filename):
+        name, ext = os.path.splitext(filename)
+        name = base64.urlsafe_b64encode(uuid.uuid4().bytes).decode('utf-8').replace('=', '')
+        return os.path.join(path, f"{name}{ext}")
+    return _func
+
+
 class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -26,6 +42,6 @@ class VideoLike(models.Model):
 
 class UserAvatar(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', default='avatars/default_avatar.png')
+    avatar = models.ImageField(upload_to=unique_filename('avatars/'), default='avatars/default_avatar.png')
     def __str__(self):
         return self.avatar.name
