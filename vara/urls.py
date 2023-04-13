fd234fc9faac6c07b55cadf0752472726c2f48f3
Niwaiwa <po2846@gmail.com>
2023-04-13 22:53:31 +0800

    add root path redirect to myapp

--- vara/urls.py
+++ vara/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/myapp/')),
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
